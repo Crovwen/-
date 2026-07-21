ea6a62745db1afd80225abda7dfbd1b7481c7a3b
Mobin <mobinking18@yahoo.com>
2026-07-21 15:34:18 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,11 @@
-# config.py
 import os
 
 class Config:
-    # توکن ربات (مستقیماً اینجا تغییر دهید)
-    BOT_TOKEN = "8915187134:AAEpyyCEh9lHszxC5PjPzuOsb8VCVDfz9BA"
-
-    # یوزرنیم کانال اجباری (بدون @)
+    BOT_TOKEN = "8915187134:AAEpyyCEh9lHszxC5PjPzuOsb8VCVDfz9BA"  # توکن ربات را اینجا بگذارید
     REQUIRED_CHANNEL = "Transit_game"
-
-    # مسیر دیتابیس SQLite
     DATABASE_PATH = os.path.join(os.path.dirname(__file__), "database", "game.db")
-
-    # تنظیمات بازی
-    REFERRAL_REWARD_REFERRER = 5000      # سکه
-    REFERRAL_REWARD_NEW_USER = 2000      # سکه
-    HOURLY_QUIZ_REWARD = 1000            # سکه (برای آینده)
-
-    # پیام‌های ثابت
+    REFERRAL_REWARD_REFERRER = 5000
+    REFERRAL_REWARD_NEW_USER = 2000
+    HOURLY_QUIZ_REWARD = 1000
     CHANNEL_NAME_DISPLAY = "𝐓𝐫𝐚𝐧𝐬𝐢𝐭 | ترانزیت"
+    INITIAL_BALANCE = 10000  # سکه شروع
