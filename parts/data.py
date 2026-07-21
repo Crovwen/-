15ac9c89bbc05180133de0ea2a69b0d656cdd1db
Mobin <mobinking18@yahoo.com>
2026-07-21 15:53:28 +0330

    Create data.py

---
+++ parts/data.py
@@ -0,0 +1,9 @@
+PART_UPGRADE_COST = {
+    "engine": 5000,
+    "gearbox": 4000,
+    "tire": 3000,
+    "brake": 3000,
+    "air_filter": 1500,
+    "oil_filter": 1500
+}
+PART_REPAIR_COST_PER_POINT = 20  # هر 1% سلامت 20 سکه
